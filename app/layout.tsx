7f73cb66454e7c56c869bb0ba3ece48f344cd35c
kimhyerim <kimhr4@visang.com>
2026-02-25 22:02:39 +0900

    Feat. Sass 설정, /chat 경로 준비

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/src/styles/globals.scss";
 
 export const metadata: Metadata = {
   title: "Hamster Bot",
-  description: "귀여운 햄스터와 대화해봐",
+  description: "귀여운 햄스터와 대화하기",
 };
 
 export default function RootLayout({
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    sourceMap: true,
+    sourceMapEmbed: true,
+  },
 };
 
 export default nextConfig;
